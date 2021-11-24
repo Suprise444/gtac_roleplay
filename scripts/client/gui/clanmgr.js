9083796fa7548ca79075b9b6363303208fad911e
Vortrex <3858226+VortrexFTW@users.noreply.github.com>
2021-11-24 07:25:35 -0600

    Clear branch

--- scripts/client/gui/clanmgr.js
+++
@@ -1,8 +0,0 @@
-// ===========================================================================
-// Vortrex's Roleplay Resource
-// https://github.com/VortrexFTW/gtac_roleplay
-// ===========================================================================
-// FILE: clanmgr.js
-// DESC: Provides clan manager GUI
-// TYPE: Client (JavaScript)
-// ===========================================================================
